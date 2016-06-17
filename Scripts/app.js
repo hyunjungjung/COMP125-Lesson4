4a5ad152302d92be4c1107eb4f9c54bc5b576fef
hyunjungjung <hjung3@my.centennialcollege.ca>
2016-06-17 01:07:41 -0400

    Updated app.js to work with multiple pages

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,15 @@
 /* Main JavaScript file */
 // global scope
-
+/**
+ * File Name : app.js
+ * 
+ * @Author Joanne Jung
+ * @date June 17, 2016
+ * 
+ * StudentID : 300432364
+ * Website : http://comp125-lesson4-joanne.azurewebsites.net
+ * @description : This file is the main JavaScript file for the website
+ */
 
 //IIFE - Immediately Invoked Functiona Expression
 (function () {
@@ -18,6 +27,54 @@
 
     // define your paragraphs array;
     var paragraphs = [];
+
+    // create a reference to the sendButton
+    var sendButton = document.getElementById("sendButton");
+
+    // check to see if sendButton exists
+    if (sendButton) {
+        // event listener
+        sendButton.addEventListener("click", sendButtonClick);
+    }
+
+    /**
+     * This function contains eventListener to submit a form
+     * with resetting function
+     * 
+     * @method sendButtonClick
+     * @returns {void} 
+     */
+    // event handler function
+    function sendButtonClick(event) {
+        console.log("clicked");
+    }
+
+    // create a reference to the firstName field
+    var firstName = document.getElementById("firstName");
+
+    // create a reference to the form
+    var contactForm = document.getElementById("contactForm");
+
+    // callback function
+    if (contactForm) {
+        // event listener with inline annoymous event handler function
+        contactForm.addEventListener("submit", function (event) {
+            event.preventDefault();
+            console.log("submmited");
+            showFormInput();
+            contactForm.reset();
+        });
+    }
+    /**
+     * This function shows the input from each form field on the console
+     * 
+     * @method showFormInput
+     * @returns {void} 
+     */
+    function showFormInput() {
+        console.log("First Name: " + firstName.value);
+    }
+
     // data for my pages   
     paragraphs[0] = "<span class='firstSentence'>This is my first paragraph. </span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu sapien, molestie vitae justo nec, placerat fermentum sem. Pellentesque vestibulum mollis sapien, eu hendrerit tellus rhoncus vel. Nullam sit amet iaculis massa, ut molestie velit. Morbi congue dolor ac purus consequat, eu scelerisque ex hendrerit. Duis tincidunt eros purus, sit amet pharetra magna eleifend ac. In hac habitasse platea dictumst. Nunc lacinia, ante et interdum consectetur, quam turpis vehicula libero, vel lobortis tortor libero ac arcu. In hac habitasse platea dictumst.";
     paragraphs[1] = "<span class='firstSentence'>This is my second paragraph. </span> Ut sit amet molestie neque, non elementum tellus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu posuere risus. Vivamus erat elit, tincidunt nec magna sit amet, eleifend suscipit ipsum. Phasellus ornare enim nec faucibus tristique. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce nec elementum tortor. Duis malesuada est ex, a pretium augue pharetra et. Nullam aliquam purus nec interdum tincidunt. Etiam eu nisi nulla. Donec ullamcorper, lectus tincidunt euismod dictum, massa arcu porta est, eu semper dui ligula at urna. Aliquam et enim interdum tellus gravida ornare.";
@@ -25,14 +82,15 @@
 
     // check to see if paragraph one exists
     var paragraphElementsLength = paragraphElements.length;
+    // if paragraph one exists, then populate each paragraph on the page
     for (var index = paragraphElementsLength; index >= 0; index--) {
-        console.log("this is log");
-        console.debug("This is debug");
-        console.error("This is an error!");
-        console.info("This is info");
-        console.warn("This is warning");
-        console.assert(index<3 ,"index out of bounds");
-             if (paragraphElements[index]) {
+        //console.log("this is log");
+        //console.debug("This is debug");
+        //console.error("This is an error!");
+        //console.info("This is info");
+        //console.warn("This is warning");
+        //console.assert(index<3 ,"index out of bounds");
+        if (paragraphElements[index]) {
             paragraphElements[index].innerHTML = paragraphs[index];
         }
     }
